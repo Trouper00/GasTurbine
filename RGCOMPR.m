e8ffb10f1a1424c7e73ee6af5e1a58dda7b7da2d
Trouper00 <165286766+Trouper00@users.noreply.github.com>
2024-03-29 02:19:12 +0300

    Update RGCOMPR.m

--- RGCOMPR.m
+++ RGCOMPR.m
@@ -8,7 +8,7 @@
                 [cp_total, R_total, gama_total, a_total, h_total, Pr_total] = FAIR_1(Tt,f);
                 T = Tt / Tt_over_T_known;
                 [cp, R, gama , a,h,Pr] = FAIR_1(T,f);
-                V2 = 2 * (h_total - h) * 9.81;
+                V2 = 2000 * (h_total - h) ;
                 if V2 < 0
                     M = 0;
                     T = Tt;
@@ -22,7 +22,7 @@
                 [cp_total, R_total, gama_total, a_total, h_total, Pr_total] = FAIR_1(Tt,f);
                 Pr = Pr_total / Pt_over_P_known;
                 [cp,R,gama,a,T,h] = FAIR(3, f, Tt, h_total, Pr);
-                V2 = 2 * (h_total - h) * 9.81;
+                V2 = 2000 * (h_total - h) ;
                 if V2 < 0
                     M = 0;
                     T = Tt;
